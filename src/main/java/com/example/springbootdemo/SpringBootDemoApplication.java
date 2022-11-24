3a561dc0e43f3f418b3a19787f71e69c744a6813
pvpablo <66192210+pvpablo@users.noreply.github.com>
2022-11-23 18:58:25 -0600

    Update SpringBootDemoApplication.java

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringBootDemoApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World v1.1";
+		return "Hello Docker World v1.2";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
